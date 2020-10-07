7af34998d181ec31bcd7c5b782c90ebfd85bae22
marcellinodour <marcellinodour@gmail.com>
2020-10-07 10:42:11 +0200

    first commit td01

---
+++ shapes/src/main/java/fr/dauphine/ja/dourmarcellino/shapes/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.dourmarcellino.shapes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
